c667d40feaea5b2692103cc51fa3bb90ac4cfe0c
Anderson <andersonbonde@gmail.com>
2024-06-30 11:49:50 -0300

    Define user Schema for database

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const validateEmail = (email) => {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+}
+
+const UserSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true,
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+  },
+  hash: { type: String, required: true },
+  salt: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
