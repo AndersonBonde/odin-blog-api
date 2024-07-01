3eab99fe90d249a94a703a0404a703ba97526739
Anderson <andersonbonde@gmail.com>
2024-07-01 10:23:47 -0300

    Add luxon package for easier Date formatting

--- models/blogPost.js
+++ models/blogPost.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const BlogPostSchema = new Schema({
   title: { type: String, required: true },
@@ -7,6 +8,12 @@ const BlogPostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   timestamp: { type: Date, required: true },
   isPublished: { type: Boolean, default: false },
+});
+
+BlogPostSchema.virtual('timestamp_formatted').get(function() {
+  return this.timestamp
+    ? DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)
+    : '';
 });
 
 module.exports = mongoose.model('BlogPost', BlogPostSchema);
