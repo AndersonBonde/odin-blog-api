f9ac6ceb06c907d551b44b8b942db04b292e668a
Anderson <andersonbonde@gmail.com>
2024-07-04 12:14:34 -0300

    Refactor from local to passport-jwt

--- app.js
+++ app.js
@@ -18,45 +18,6 @@ require('./config/database');
 const passport = require('passport');
 require('./config/passport')(passport);
 
-// --- Passport LocalStrategy & serialization setup.
-// const LocalStrategy = require('passport-local').Strategy;
-// const User = require('./models/user');
-// const { validatePassword } = require('./lib/passwordUtils');
-
-// const strategy = new LocalStrategy({
-//   usernameField: 'email',
-//   passwordField: 'password',
-// },
-// (email, password, done) => {
-//   User.findOne({ email })
-//     .then((user) => {
-//       if (!user) return done(null, false);
-
-//       const { hash, salt } = user;
-//       const isValid = validatePassword(password, hash, salt);
-
-//       if (isValid) {
-//         return done(null, user);
-//       } else {
-//         return done(null, false);
-//       }
-//     })
-//     .catch((err) => done(err));
-// });
-// passport.use(strategy);
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser((userId, done) => {
-//   User.findById(userId)
-//     .then((user) => {
-//       done(null, user);
-//     })
-//     .catch((err) => done(err));
-// });
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 const { validatePassword } = require('../lib/passwordUtils');
 
@@ -7,9 +9,23 @@ const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
 const options = {
-
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['RS256'],
 };
 
-module.exports = (passport) => {
+const strategy = new JwtStrategy(options, (payload, done) => {
+  User.findOne({ _id: payload.sub })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => done(err, null));
+});
 
+module.exports = (passport) => {
+  passport.use(strategy);
 }
